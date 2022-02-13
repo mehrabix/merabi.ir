0c54bd8ded238ecd9f592f8427ce9dc4ba1c6250
mehrabix <mehrabi@post.com>
2022-02-13 22:09:03 +0330

    dd

--- src/components/navbar.svelte
+++ src/components/navbar.svelte
@@ -49,13 +49,17 @@
 		</div>
 	</div>
 	<div class="w-[9rem] h-10 border-2 border-white rounded-md absolute left-8 xl:relative m-4 mt-7">
-		<div class="w-[9rem] h-10 border-2 border-white rounded-md absolute -top-2 -left-2">
-			<div
-				dir="ltr"
-				class="w-[138px] font-mono transition-all ease-linear duration-200 h-[34px] border-2 hover:bg-white hover:text-[#1d1d1d] cursor-pointer  border-transparent  rounded-tl-md rounded-br-md absolute top-1 left-1 flex justify-center items-center font-bold"
-			>
-				{'{() => MERABi}'}
+		
+		<div class="flex">
+			<div class="w-[9rem] h-10 border-2 border-white rounded-md absolute -top-2 -left-2">
+				<div
+					dir="ltr"
+					class="w-[138px] font-mono transition-all ease-linear duration-200 h-[34px] border-2 hover:bg-white hover:text-[#1d1d1d] cursor-pointer  border-transparent  rounded-tl-md rounded-br-md absolute top-1 left-1 flex justify-center items-center font-bold"
+				>
+					{'{() => MERABi}'}
+				</div>
 			</div>
+			<button class="absolute -right-14 mt-1 focus:text-yellow-600"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-sun"><circle cx="12" cy="12" r="5"></circle><line x1="12" y1="1" x2="12" y2="3"></line><line x1="12" y1="21" x2="12" y2="23"></line><line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line><line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line><line x1="1" y1="12" x2="3" y2="12"></line><line x1="21" y1="12" x2="23" y2="12"></line><line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line><line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line></svg></button>
 		</div>
 	</div>
 </div>
