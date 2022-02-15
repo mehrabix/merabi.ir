e4926696ff2915e05f4b85cb16deee889fa51419
mehrabix <mehrabi@post.com>
2022-02-15 19:23:02 +0330

    refacot

--- src/components/navbar.svelte
+++ src/components/navbar.svelte
@@ -1,65 +1,85 @@
-<script>
-	let _navbarToggle = false;
-	const NavToggle = () => {
-		if (!_navbarToggle) {
-			_navbarToggle = true;
-		} else {
-			_navbarToggle = false;
-		}
-	};
-</script>
-
-<div class="flex justify-between items-start lg:items-center  text-white lg:p-4">
-	<div class="flex   flex-col lg:items-center lg:flex-row w-full md:w-10/12">
-		<div on:click={NavToggle} class="h-11 w-11 z-50 cursor-pointer lg:hidden m-4 mt-6 mr-8">
-			<img class="h-10" src="/assets/images/menu.svg" alt="" />
-		</div>
-		<div class="absolute w-full lg:relative bg-[#00000057] lg:bg-transparent">
-			<ul
-				id="nav"
-				class="  lg:visible transition-all  lg:mt-auto  flex {_navbarToggle
-					? 'h-[100vh]'
-					: 'invisible h-0'}   w-full lg:w-8/12  items-center lg:h-auto justify-evenly font-Merriweather-Black flex-col lg:flex-row"
-			>
-				<li
-					class="w-32 h-8 flex justify-center items-center transition-all ease-linear duration-150 hover:bg-[#e720667e]"
-				>
-					<!-- svelte-ignore a11y-invalid-attribute -->
-					<a href="#">About</a>
-				</li>
-				<li
-					class="w-32 h-8 flex justify-center items-center transition-all ease-linear duration-150 hover:bg-[#e720667e]"
-				>
-					<!-- svelte-ignore a11y-invalid-attribute -->
-					<a href="#">Writing</a>
-				</li>
-				<li
-					class="w-32 h-8 flex justify-center items-center transition-all ease-linear duration-150 hover:bg-[#e720667e]"
-				>
-					<!-- svelte-ignore a11y-invalid-attribute -->
-					<a href="#">Portfolio</a>
-				</li>
-				<li
-					class="w-32 h-8 flex justify-center items-center transition-all ease-linear duration-150 hover:bg-[#e720667e]"
-				>
-					<!-- svelte-ignore a11y-invalid-attribute -->
-					<a href="#">Resume</a>
-				</li>
-			</ul>
-		</div>
-	</div>
-	<div class="w-[9rem] h-10 border-2 border-white rounded-md absolute left-8 xl:relative m-4 mt-7">
-		
-		<div class="flex">
-			<div class="w-[9rem] h-10 border-2 border-white rounded-md absolute -top-2 -left-2">
-				<div
-					dir="ltr"
-					class="w-[138px] font-mono transition-all ease-linear duration-200 h-[34px] border-2 hover:bg-white hover:text-[#1d1d1d] cursor-pointer  border-transparent  rounded-tl-md rounded-br-md absolute top-1 left-1 flex justify-center items-center font-bold"
-				>
-					{'{() => MERABi}'}
+<div class="px-5 pt-5">
+	<div class=" flex justify-between items-start ">
+		<div class="flex justify-between items-center w-full flex-col lg:flex-row flex-wrap">
+			<div class="w-full lg:w-auto flex flex-row-reverse  justify-between">
+				<button><img class="h-10 mt-1 lg:hidden" src="/assets/images/menu.svg" alt="" /></button>
+				<div class="flex">
+					<div class="flex w-[24vh] justify-between">
+						<div class="w-28 h-10 border-2 border-white rounded-lg relative">
+							<div class="w-28 h-10 border-2 border-white rounded-lg  left-1 absolute  top-1">
+								<div class="relative">
+									<div
+										class=" right-1 w-[106px] -mt-[1px] h-8 transition-all ease-linear duration-200 hover:bg-slate-50 cursor-pointer text-white hover:text-black   rounded-tl-md rounded-br-md    absolute  "
+									>
+										<div class="relative flex  justify-center items-center h-full">
+											<h1 class="text-xs    font-mono">{'{() => MERABi}'}</h1>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<button class="  mt-1 focus:text-yellow-600 text-white"
+						><svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="24"
+							height="24"
+							viewBox="0 0 24 24"
+							fill="none"
+							stroke="currentColor"
+							stroke-width="2"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							class="feather feather-sun"
+							><circle cx="12" cy="12" r="5" /><line x1="12" y1="1" x2="12" y2="3" /><line
+								x1="12"
+								y1="21"
+								x2="12"
+								y2="23"
+							/><line x1="4.22" y1="4.22" x2="5.64" y2="5.64" /><line
+								x1="18.36"
+								y1="18.36"
+								x2="19.78"
+								y2="19.78"
+							/><line x1="1" y1="12" x2="3" y2="12" /><line x1="21" y1="12" x2="23" y2="12" /><line
+								x1="4.22"
+								y1="19.78"
+								x2="5.64"
+								y2="18.36"
+							/><line x1="18.36" y1="5.64" x2="19.78" y2="4.22" /></svg
+						></button
+					>
+				
 				</div>
 			</div>
-			<button class="absolute -right-14 mt-1 focus:text-yellow-600"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-sun"><circle cx="12" cy="12" r="5"></circle><line x1="12" y1="1" x2="12" y2="3"></line><line x1="12" y1="21" x2="12" y2="23"></line><line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line><line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line><line x1="1" y1="12" x2="3" y2="12"></line><line x1="21" y1="12" x2="23" y2="12"></line><line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line><line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line></svg></button>
+			<div class="text-white mt-5 lg:mt-0  lg:w-auto flex items-center justify-center h-0 lg:h-auto overflow-hidden w-full">
+				<ul class="flex flex-col lg:flex-row items-center w-full">
+					<li
+						class="w-32 h-8 flex justify-center items-center transition-all ease-linear duration-150 hover:bg-[#e720667e]"
+					>
+						<!-- svelte-ignore a11y-invalid-attribute -->
+						<a href="#">About</a>
+					</li>
+					<li
+						class="w-32 h-8 flex justify-center items-center transition-all ease-linear duration-150 hover:bg-[#e720667e]"
+					>
+						<!-- svelte-ignore a11y-invalid-attribute -->
+						<a href="#">Writing</a>
+					</li>
+					<li
+						class="w-32 h-8 flex justify-center items-center transition-all ease-linear duration-150 hover:bg-[#e720667e]"
+					>
+						<!-- svelte-ignore a11y-invalid-attribute -->
+						<a href="#">Portfolio</a>
+					</li>
+					<li
+						class="w-32 h-8 flex justify-center items-center transition-all ease-linear duration-150 hover:bg-[#e720667e]"
+					>
+						<!-- svelte-ignore a11y-invalid-attribute -->
+						<a href="#">Resume</a>
+					</li>
+				</ul>
+			</div>
 		</div>
 	</div>
 </div>
