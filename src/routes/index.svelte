b708a14d08a1b37513dfe29f6ace03f0687463fd
mehrabix <mehrabi@post.com>
2022-02-17 14:52:12 +0330

    sd

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,10 +1,13 @@
 <!-- <h1>This is index</h1> -->
 
 <div class="flex flex-col mt-9 justify-center gap-9 items-center lg:flex-row px-6">
-	<img
-		class="w-60 rounded-full cursor-pointer hover:opacity-75 transition-all duration-1000 ease-in-out hover:rotate-[360deg]"
-		src="/static/assets/images/ahmad-mehrabi.jpg"
-		alt="Ahmad Mehrabi" />
+	<div class="relative hover:opacity-75 transition-all duration-1000 ease-in-out">
+		<img
+			class="w-60 rounded-full    "
+			src="/static/assets/images/ahmad-mehrabi.jpg"
+			alt="Ahmad Mehrabi" />
+		<div class="absolute w-full h-full border-x-4 top-0 rounded-full border-black animate-pulse" />
+	</div>
 	<div class="flex flex-col gap-y-3 items-center justify-center text-center">
 		<div
 			class="text-white leading-snug font-black text-5xl flex flex-wrap gap-x-4 items-center
@@ -96,11 +99,22 @@
 			</a>
 			<a href="https://t.me/ahmadmehrabix">
 
-				
-
-				<svg class="fill-white hover:fill-[#000] transition-all duration-300 ease-in-out"  width="35px" height="35px" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="currentColor" >
-					<path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
-				  </svg>
+				<svg
+					class="fill-white hover:fill-[#000] transition-all duration-300 ease-in-out"
+					width="35px"
+					height="35px"
+					viewBox="0 0 16 16"
+					xmlns="http://www.w3.org/2000/svg"
+					fill="currentColor">
+					<path
+						d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38
+						0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01
+						1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95
+						0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27
+						2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82
+						1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0
+						.21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
+				</svg>
 
 			</a>
 		</div>
